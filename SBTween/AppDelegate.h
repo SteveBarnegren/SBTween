e39a41eed0916faba3e3caf620374b3d50f96868
Steve Barnegren <steve.barnegren@himumsaiddad.com>
2016-03-15 12:14:03 +0000

    Initial Commit

---
+++ SBTween/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SBTween
+//
+//  Created by Steven Barnegren on 15/03/2016.
+//  Copyright © 2016 Steve Barnegren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
